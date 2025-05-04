1d38f976742c966ebca175b68f46fa12c72d138b
Kimurist0415 <ryuhki2003@gmail.com>
2025-05-04 15:58:52 +0900

    WIP: 一時コミット（pullのため）

--- config/asgi.py
+++ config/asgi.py
@@ -8,20 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import community_app.routing  # WebSocketルーティング定義ファイルを読み込む
+from channels.routing import ProtocolTypeRouter, URLRouter
+from community_app.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # 従来のHTTPリクエスト
-    "websocket": AuthMiddlewareStack(  # WebSocketリクエスト
-        URLRouter(
-            community_app.routing.websocket_urlpatterns  # ルーティング一覧を指定
-        )
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
+

3a35617058a50598c8f676ebe8e3b044ba2ddc28
Kimurist0415 <ryuhki2003@gmail.com>
2025-05-04 16:42:58 +0900

    Add: Enable HTTP/2 support for Daphne via Twisted extras

--- community_app/consumers.py
+++ community_app/consumers.py
@@ -5,7 +5,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_group_name = "chat_room"
+        # URLからroom_nameを取得
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
 
         # グループに参加
         await self.channel_layer.group_add(
